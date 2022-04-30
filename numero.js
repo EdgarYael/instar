60b9572b0c51c994321f870922e11c8ef3c8517e
EdgarYael <edgaryaelcardenas0u@gmail.com>
2022-04-30 14:43:22 -0500

    commit inicial

---
+++ numero.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var numero = parseInt(prompt(" Escribe el primer numero"));
+var numero2 = parseInt(prompt(" Escribe el primer numero"));
+
+var suma = numero + numero2;
+
+
+console.log(suma);
